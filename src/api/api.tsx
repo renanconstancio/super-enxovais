fa48134061a76d53219b1628bbb6b8bd8bf6383b
Renan Constancio <renanhenrique86@hotmail.com>
2021-12-31 11:57:27 -0300

    aaaaa

--- src/api/api.tsx
+++ src/api/api.tsx
@@ -1,9 +1,12 @@
 import axios from 'axios';
 
+console.log('process.env.PORT', process.env.PORT);
+
 // axios.defaults.headers.get['Access-Control-Allow-Origin'] = '*';
 // eslint-disable-next-line no-undef
 // axios.defaults.baseURL = process.env.REACT_APP_URI_API;
 // axios.defaults.baseURL = `http://localhost:${process.env.PORT || 3000}`;
+// axios.defaults.baseURL = `https://bling.com.br/Api/v2`;
 axios.defaults.timeout = 5000;
 
 const api = axios.create();
