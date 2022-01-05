89889f30695a5acde803c2e27acda5f857d9ef50
Renan Constancio <renanhenrique86@hotmail.com>
2022-01-05 17:07:27 -0300

    Remoção de componente

--- src/components/Titles/index.tsx
+++
@@ -1,9 +0,0 @@
-type TTitles = {
-  texto?: string;
-  classe: string;
-};
-const Titles = ({ texto, classe }: TTitles) => {
-  return <div className={classe}>{texto}</div>;
-};
-
-export default Titles;
